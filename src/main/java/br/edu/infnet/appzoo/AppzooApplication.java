ee47bf2d2e6b243f497d5aa514bdb685baf09b3e
carraes <carraeshb@gmail.com>
2023-10-06 23:36:20 -0300

    chore: initial commit

---
+++ src/main/java/br/edu/infnet/appzoo/AppzooApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appzoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppzooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppzooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/appzoo/AppzooApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appzoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppzooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
