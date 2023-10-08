38f49fb5b9ec9f97508937e3a7b933675406f2ff
carraes <carraeshb@gmail.com>
2023-10-08 00:15:38 -0300

    feat: create service

---
+++ src/main/java/br/edu/infnet/appzoo/service/ManagerService.java
@@ -0,0 +1,33 @@
+package br.edu.infnet.appzoo.service;
+
+import br.edu.infnet.appzoo.models.Manager;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ManagerService {
+  private final Map<UUID, Manager> managers = new HashMap<>();
+
+  public void addManager(Manager manager) {
+    managers.put(manager.getId(), manager);
+  }
+
+  public Collection<Manager> getAllManagers() {
+    return managers.values();
+  }
+
+  public List<Map<String, String>> convertManagersToMaps(Collection<Manager> managers) {
+    return managers.stream().map(Manager::toMap).collect(Collectors.toList());
+  }
+
+  public List<Map<String, String>> getManagerMaps() {
+    Collection<Manager> managersList = getAllManagers();
+    return convertManagersToMaps(new ArrayList<>(managersList));
+  }
+}
