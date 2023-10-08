27f5942ff30c601487bfcaf079f1a1da868bb852
carraes <carraeshb@gmail.com>
2023-10-08 01:11:47 -0300

    feat: create login routing

--- src/main/java/br/edu/infnet/appzoo/controller/AppController.java
+++ src/main/java/br/edu/infnet/appzoo/controller/AppController.java
@@ -2,11 +2,27 @@ package br.edu.infnet.appzoo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppController {
   @GetMapping(value = "/")
   public String telaHome() {
     return "home";
+  }
+
+  @GetMapping(value = "/login")
+  public String telaLogin() {
+    return "login";
+  }
+
+  @PostMapping(value = "/login")
+  public String login(@RequestParam String email, @RequestParam String password) {
+    if (email.equals("admin@admin.com") && password.equals("123456")) {
+      return "redirect:/";
+    }
+
+    return "redirect:/login";
   }
 }
--- src/main/java/br/edu/infnet/appzoo/controller/ManagerController.java
+++ src/main/java/br/edu/infnet/appzoo/controller/ManagerController.java
@@ -1,14 +1,19 @@
 package br.edu.infnet.appzoo.controller;
 
+import br.edu.infnet.appzoo.models.Manager;
 import br.edu.infnet.appzoo.service.ManagerService;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ManagerController {
@@ -26,6 +31,16 @@ public class ManagerController {
   @DeleteMapping("/managers/delete/{id}")
   public String deleteManager(@PathVariable UUID id) {
     managerService.delete(id);
+    return "redirect:/managers/list";
+  }
+
+  @PostMapping("/managers/create")
+  public String createManager(
+      @RequestParam String name,
+      @RequestParam String email,
+      @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate birthDate) {
+
+    managerService.create(new Manager(name, email, birthDate));
     return "redirect:/managers/list";
   }
 }
